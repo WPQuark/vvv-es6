f7664696f619b7632a02321f498114710ea4fcd1
Swashata Ghosh <swashata@wpquark.com>
2018-03-07 16:14:49 +0530

    feat: Add provision files

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # VVV ES6 site template
 
 For when you just need the nginx server and practically no PHP. Although PHP
-is included for having AJAX or REST related stuff.
+is included for having AJAX or REST related stuff. Also a database is created
+in case you need to do something sassy.
 
 This is a good way to run your JS apps from the same vagrant as your VVV without
 installing WordPress.
+
+Instead of WordPress we install a simple PHP [directory lister](https://github.com/halgatewood/file-directory-list).
 
 ## Configuration
 
@@ -18,3 +21,21 @@ es6:
 ```
 
 Then provision the box and you are done.
+
+## Configuration Options
+
+```
+hosts:
+    - foo.test
+    - bar.test
+    - baz.test
+```
+Defines the domains and hosts for VVV to listen on.
+The first domain in this list is your sites primary domain.
+
+```
+custom:
+    db_name: super_secet_db_name
+```
+Defines the DB name for the installation.
+
---
+++ provision/vvv-init.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Provision a simple Nginx server with PHP support
+
+DOMAIN=`get_primary_host "${VVV_SITE_NAME}".test`
+DOMAINS=`get_hosts "${DOMAIN}"`
+SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
+DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
+DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
+
+# Make a database, if we don't already have one
+echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
+echo -e "\n DB operations done.\n\n"
+
+# Nginx Logs
+mkdir -p ${VVV_PATH_TO_SITE}/log
+touch ${VVV_PATH_TO_SITE}/log/error.log
+touch ${VVV_PATH_TO_SITE}/log/access.log
+
+# Install the PHP directory indexer
+if [[ ! -f "${VVV_PATH_TO_SITE}/public_html/index.php" ]]; then
+	mkdir -p "${VVV_PATH_TO_SITE}/public_html" 2> /dev/null
+	cp -f "${VVV_PATH_TO_SITE}/provision/index.php" "${VVV_PATH_TO_SITE}/public_html/index.php"
+	sed -i "s#{{SITE_TITLE_HERE}}#${SITE_TITLE}#" "${VVV_PATH_TO_SITE}/public_html/index.php"
+fi
+
+# Copy the nginx configuration
+cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+sed -i "s#{{DOMAINS_HERE}}#${DOMAINS}#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+
+if [ -n "$(type -t is_utility_installed)" ] && [ "$(type -t is_utility_installed)" = function ] && `is_utility_installed core tls-ca`; then
+    sed -i "s#{{TLS_CERT}}#ssl_certificate /vagrant/certificates/${VVV_SITE_NAME}/dev.crt;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+    sed -i "s#{{TLS_KEY}}#ssl_certificate_key /vagrant/certificates/${VVV_SITE_NAME}/dev.key;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+else
+    sed -i "s#{{TLS_CERT}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+    sed -i "s#{{TLS_KEY}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+fi
